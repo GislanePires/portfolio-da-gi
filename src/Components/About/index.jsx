e69a2a7c612995b913e95bb52f728678d4a3f7a2
GislanePires <piresgislane7@gmail.com>
2024-11-04 05:01:44 -0300

    feat: ancoragem das li da header

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,34 +1,226 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { SlSocialGithub, SlSocialLinkedin } from "react-icons/sl";
 
-const AboutSection = styled.section`
-width: 100%;
-height: 40vh;
-background-color: rgb(0, 0, 0);
-display: flex;
-justify-content: space-around;
-align-items: center;
-padding: 8rem;
-img {
-    width: 20rem;
-    height: 20rem;
-    border-radius: 100%;
+const blinkCursor = keyframes`
+    from {
+        border-right-color: rgba(255, 255, 255, .75);
+    }
     
-}
+    to {
+        border-right-color: transparent;
+    }
+`;
+const blinkCursor2 = keyframes`
+    from {
+        border-right-color: transparent;
+    }
+    
+    to {
+        border-right-color: rgba(255, 255, 255, .75);
+    }
+`;
+const typing = keyframes`
+    from {
+        width: 0;
+        height: 1.2em;
+    }
+    to {
+        width: 13em;
+        height: 1.2em;
+    }
+`;
+const typing2 = keyframes`
+    from {
+        width: 0;
+        height: 1.2em;
+    }
+    to {
+        width: 15em;
+        height: 1.2em;
+    }
+
+
+`;
+const AboutSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(0, 0, 0);
 `;
 const AboutText = styled.article`
-width: 100%;
-height: 100%;
-color: rgba(255, 255, 255, .75);
-font-family: 'Special Elite', monospace;
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 16vh;
+  color: rgba(255, 255, 255, 0.75);
+  font-family: "Special Elite", monospace;
 `;
-export default function About(){
-    return (
-        <AboutSection>
-            <AboutText>
-                <p>Olá, eu sou a Gislane Lima! Mas pode me chamar de Gisa...</p>
-            </AboutText>
-            <img src="./perfil.jpg" alt="perfil" />
-        </AboutSection>
-    );
+const Line1 = styled.p`
+  font-size: 1.6em;
+  margin: 0 auto;
+  border-right: 2px solid rgba(80, 21, 109, 0.75);
+  animation: ${blinkCursor} 5s steps(3) normal both,
+    ${typing} 5s steps(40) normal both;
+  white-space: nowrap;
+  overflow: hidden;
+  height: 1.2em;
+  text-align: center;
+`;
+const Line2 = styled.p`
+  font-size: 1.6em;
+  margin: 0 auto;
+  border-right: 2px solid rgba(68, 15, 95, 0.75);
+  animation: ${blinkCursor2} 1s steps(3) infinite normal both,
+    ${typing2} 5s steps(40) normal both;
+  white-space: nowrap;
+  overflow: hidden;
+  height: 1.2em;
+  text-align: center;
+  animation-delay: 5s;
+`;
+const AboutPerfil = styled.section`
+  display: flex;
+  width: 100%;
+  height: 40vh;
+  background-color: rgb(0, 0, 0);
+  justify-content: space-between;
+  align-items: center;
+  padding: 8rem;
+  /* border: solid 2px pink; */
+  img {
+    width: 20rem;
+    height: 18rem;
+    border-radius: 100%;
+  }
+`;
+const MainAbout = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
+  color: rgba(255, 255, 255, 0.75);
+  width: 60vw;
+  height: 80vh;
+  background-color: #151515;
+  padding: 3rem;
+  border-radius: 0.3rem;
+  border: solid 2px #242424;
+  font-family: "Anek Devanagari", sans-serif;
+ 
+
+  img {
+    width: 30vw;
+    height: 50vh;
+    border-radius: 0.3rem;
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+  }
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 12vh;
+
+    button {
+      color: #966cfc;
+      width: 6rem;
+      height: 4rem;
+      border: solid 2px #242424;
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      background-color: #111111;
+      cursor: pointer;
+      position: relative;
+      bottom: 1.2rem;
+      transition: all 0.8s ease;
+        position: relative;
+        overflow: hidden;
+
+        &:hover {
+            transform: translateY(-5px);
+            border: solid 2px #966CFC;
+        }
+    }
+  }
+`;
+const GithubIcon = styled(SlSocialGithub)`
+  width: 4rem;
+  height: 2.5rem;
+`;
+const LinkedinIcon = styled(SlSocialLinkedin)`
+  width: 4rem;
+  height: 2.5rem;
+`;
+const ContainerImg = styled.section`
+  display: flex;
+  align-items: center;
+`;
+const MainText = styled.article`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  width: 100%;
+  height: 90%;
+  justify-content: space-around;
+  h3 {
+    color: #966cfc;
+}
+h2 {
+    font-size: 2.5rem;
+}
+p {
+      padding-bottom: 2rem;
+
+  }
+`;
+const Emphasis = styled.p`
+    /* font-weight: 600; */
+    font-size: 1.3em;
+`;
+
+export default function About() {
+  return (
+    <AboutSection id="sobre">
+      <AboutPerfil>
+        <AboutText>
+          <Line1>Olá, sou a Gislane Lima!</Line1>
+          <Line2>Mas pode me chamar de Gisa...</Line2>
+        </AboutText>
+        <img src="./perfil.jpg" alt="perfil" />
+      </AboutPerfil>
+      <MainAbout>
+        <ContainerImg>
+          <img src="./girl-developer.jpg" alt="" />
+          <MainText>
+            <h3>Quem sou</h3>
+            <h2>Gislane Lima</h2>
+            <Emphasis>Frontend Developer</Emphasis>
+            <p>
+              Meu nome é Gislane Lima, ou "Gisa" - como a maioria das pessoas me
+              conhece. Atualmente sou estudante de Desenvolvimento de sistemas e
+              UI Designer. Gosto de desenolver interfaces interativas,
+              responsivas e modernas
+            </p>
+        <div>
+          <button
+          onClick="https://github.com/GislanePires"
+          >
+            <GithubIcon />
+          </button>
+          <button>
+            <LinkedinIcon />
+          </button>
+        </div>
+          </MainText>
+        </ContainerImg>
+      </MainAbout>
+    </AboutSection>
+  );
 }
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,30 +1,59 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const AboutHeader = styled.section`
-
-width: 100%;
-height: 50vh;
- background-image: 
- linear-gradient(
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.5)
-      ), 
-      url("./bg1.jpg");
-    /* box-shadow: 300px 500px 60px 100px rgb(0, 0, 0); */
-    background-size: cover;
-    background-position: center;
-    /* box-shadow: 0px -83px 77px 23px rgba(0,0,0,1) inset;
-    -webkit-box-shadow: 0px -83px 77px 23px rgba(0,0,0,1) inset;
-    -moz-box-shadow: 0px -83px 77px 23px rgba(0,0,0,1) inset; */
-    box-shadow: 0px -83px 131px 65px rgba(0,0,0,1) inset;
-    -webkit-box-shadow: 0px -83px 131px 65px rgba(0,0,0,1) inset;
-    -moz-box-shadow: 0px -83px 131px 65px rgba(0,0,0,1) inset;
+  width: 100%;
+  height: 50vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("./bg1.jpg");
+  background-size: cover;
+  background-position: center;
+  box-shadow: 0px -83px 131px 65px rgba(0, 0, 0, 1) inset;
+  -webkit-box-shadow: 0px -83px 131px 65px rgba(0, 0, 0, 1) inset;
+  -moz-box-shadow: 0px -83px 131px 65px rgba(0, 0, 0, 1) inset;
 `;
-export default function Header() {
-    return(
-        <AboutHeader>
+const NavList = styled.ul`
+/* border: solid 1px blue; */
+display: flex;
+justify-content: space-around;
+align-items: center;
+height: 12vh;
+padding-top: 4rem;
 
-            <h1>Header</h1>
-        </AboutHeader>
-    );
+li {
+    list-style: none;
+    font-family: "Anek Devanagari", sans-serif;
+    font-size: 1.5rem;
+    font-weight: 800;
+    
+    a {
+        color: aliceblue;
+        text-decoration:none;
+
+    }
+
+}
+`;
+
+export default function Header() {
+  return (
+    <AboutHeader>
+      <nav>
+        <NavList>
+          <li>
+          <a href="#sobre">Sobre</a>
+          </li>
+          <li>
+          <a href="#experiencias">Experiências</a>
+          </li>
+          <li>
+            <a href="#projetos">Projetos</a>
+          </li>
+          <li>
+            <a href="#conhecimentos">Conhecimentos</a>
+          </li>
+        </NavList>
+      </nav>
+    </AboutHeader>
+  );
 }
